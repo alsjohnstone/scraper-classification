4e57088d4e60b9e81cde2cae7f2f01fc173dd4ef
Alistair <summermakegood@hotmail.com>
2020-07-29 11:07:18 +0100

    Primary, subcategory and secondary subcategories

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-/*
-to do
-- add config.json to gcp bucket
-- shell script for startup
-*/
-
 const HCCrawler = require("headless-chrome-crawler");
 const { google } = require("googleapis");
 const scopes = "https://www.googleapis.com/auth/analytics";
 const credentials = "./credentials.json";
 const key = require("./credentials.json");
 const config = require("./config.json");
+
+var pagesCrawled = 0;
 
 const jwt = new google.auth.JWT(
   key.client_email,
@@ -18,13 +14,13 @@ const jwt = new google.auth.JWT(
   scopes
 );
 
-let pageCount = 1;
+var pageCount = 0;
 
 // NLP results for GA Data Import
 const nlpResults = [config.dataImportSchema];
 
 // Upload classification data to Google Analytics
-async function uploadData() {
+const uploadData = async () => {
   const response = await jwt.authorize();
   const result = await google.analytics("v3").management.uploads.uploadData({
     auth: jwt,
@@ -36,17 +32,18 @@ async function uploadData() {
       "Content-Type": "application/octet-stream",
     },
     media: {
-      body: nlpResults.join("\n"),
+      body: [...new Set(nlpResults)].join("\n"),
       MimeType: "application/octet-stream",
     },
   });
   if (result.status == 200) {
     console.log("Classifications uploaded to GA");
   }
-}
+};
 
 // Run the page text through classification API
-async function returnCategories(result) {
+const returnCategories = async (result) => {
+  pagesCrawled++;
   try {
     if (!result.result.pagePath.includes("our-blog")) {
       const language = require("@google-cloud/language");
@@ -63,26 +60,28 @@ async function returnCategories(result) {
       };
       // Classifies text in the document
       const [classification] = await client.classifyText({ document });
+      pageCount++;
       // Add to nlpResults array, used to GA import
       nlpResults.push([
         result.result.pagePath,
-        classification.categories[0].name,
+        classification.categories[0].name.split("/")[1],
+        classification.categories[0].name.split("/")[2],
+        classification.categories[0].name.split("/")[3],
       ]);
       console.log(pageCount + ") Page Path: " + result.result.pagePath);
       console.log("   Category: " + classification.categories[0].name);
-      pageCount++;
     }
   } catch (error) {
-    console.log(pageCount + ") Page Path: " + result.result.pagePath);
-    console.log("   Category: NA");
-    pageCount++;
+    console.log("!!" + " Page Path: " + result.result.pagePath);
+    console.log("   NO CATEGORY FOUND");
   }
-}
+};
 
 (async () => {
   const crawler = await HCCrawler.launch({
-    //args: ["--no-sandbox"],
-    maxDepth: 9999,
+    args: ["--no-sandbox"],
+    maxConcurrency: 50,
+    maxDepth: 9999999,
     allowedDomains: config.allowedDomains, // www.naturesbest.co.uk
     // Function to be evaluated in browsers
     evaluatePage: () => ({
@@ -98,10 +97,12 @@ async function returnCategories(result) {
   });
   // Queue a request
   console.log("Crawl started");
-  await crawler.queue(config.startUrl); // https://www.naturesbest.co.uk
+  await crawler.queue(config.startUrl);
   await crawler.onIdle(); // Resolved when no queue is left
   await crawler.close(); // Close the crawler
-  // Upload CSV to GA when crawl is complete
-  console.log("Crawl complete");
+  // Upload NLP data to GA when crawl is complete
+  console.log(
+    `Crawl complete. ${pagesCrawled} pages crawled. ${pageCount} pages successfully classified`
+  );
   uploadData();
 })();
